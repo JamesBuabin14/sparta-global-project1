794ebe870580e185095e03ea875e79749e5ec925
Tech-A38 <tech-a38@Tech-A38s-MacBook-Air.local>
2018-02-09 06:43:03 +0000

    Finished game

--- README.md
+++ README.md
@@ -1,2 +1,125 @@
-# sparta-global-project1
+# Sparta-Global-Project1
+
 Matching card games
+
+## Table of Contents
+
+Code Snippets
+
+- The Software
+- Task Requirements
+- How to download and play
+- Aim of the game
+- Stages
+- Sprint 1
+
+  - Planning
+  - Wireframe
+  - Issues
+  - Images
+  - User Stories
+  - Acceptance Criteria
+
+- Sprint 2
+
+  - What I Found Difficult
+  - Successes
+
+- Website Results
+
+### Code Snippets
+
+`$(document).ready(function() { //When the page loads`
+
+`var cards = document.getElementsByClassName("card"); //The variable cards gets the class name "cards" where the list in the HTML where the class names will be stored in`
+
+`var array = [];`
+
+`var score = 0;`
+
+`var count;`
+
+`Fisher-yates shuffle method function shuffle(array) { var currentIndex = array.length, temporaryValue, randomIndex; //Gets the current array`
+
+`// While there remain elements to shuffle... while (0 !== currentIndex) { //While the array is not equalled to zero it goes through the array`
+
+### Task Requirements
+
+Was to build a browser based game that should be built using html, css and JavaScript
+
+#### Important Requirements
+
+- Include an instruction section explaining how to play the game.
+- Ether display a message when the user has won or display the users score after the game has finished.
+- Be styled to perfection!
+- Be hosted online!
+
+### The Software
+
+This game built using HTML, CSS and Javascript.
+
+### How to download and play
+
+Run the file by accessing the link <https://github.com/JamesBuabin14/sparta-global-project1/> then select solo play to begin or select the the clone or download button and download zip file. Once the file is downloaded open the file **index.html** file in your browser.
+
+## Aim of the game
+
+I decided to build a matching card game as it is easy to play and can be played by children or adults with multiple people. Something that requires observation, concentration and a good memory to win. The player has to find all the matching cards by selecting a card which is randomly positioned before the time runs out.
+
+## Sprint 1
+
+### Wireframe
+
+Beginning my idea I drafted my image on paper to get an understanding of what my game will look like
+
+![alt text](img/wireframe1.jpg "Wireframe") ![alt text](img/wireframe2.jpg "Wireframe") ![alt text](img/wireframe3.jpg "Wireframe") ![alt text](img/wireframe4.jpg "Wireframe")
+
+### Issues
+
+Approaching sprint 1 I came across issues trying making the cards be positioned randomly. In order for the game to have random positioning I needed to have placed an algorithm that switch cards around every time the page loads. At the time I had no experience with algorithms however it was the only way to get the feature to work so I used the Fisher-yates shuffle algorithm.
+
+```function shuffle(array) { var currentIndex = array.length, temporaryValue, randomIndex; //Gets the current array
+
+`// While there remain elements to shuffle... while (0 !== currentIndex) { //While the array is not equalled to zero it goes through the array`
+
+`// Pick a remaining element... randomIndex = Math.floor(Math.random() * currentIndex); currentIndex -= 1; //Generates a random number each time the loop passes and saves in the current index`
+
+`// And swap it with the current element. temporaryValue = array[currentIndex]; //Gets the current index position stores in temporary value array[currentIndex] = array[randomIndex]; //The position of the random number is grabbed by the array of the current position array[randomIndex] = temporaryValue; //This switches the current index position with the array position }`
+
+`return array; //restores the card position back to the array }`
+
+### User Stories
+
+On the website Trello I create stories to understand and work towards completing an mvp (minimal viable product) helping me think of what actions to take when list the features. When using Trello I would drag the message boxes if they were in progress or if they were completed.
+
+Whilst using Trello I made sure that I worked in order of priority creating the main the tasks first the any bonus features later on.
+
+![alt text](img/Trello.png "Trello")
+
+### Acceptance Criteria
+
+My acceptance criteria was built around if it had the main things for a matching card game from score, pairing cards, timer, winner or loser functioning and displayed in a grid. I would have liked all user stories to be complete even though they were at a low priority.
+
+## Sprint 2
+
+### What I found difficult
+
+#### Matching the cards
+
+Matching the cards became quite troublesome as of thinking of a way to pair the selected cards. I overcame this problem by setting the cards with an id (of the 1st and 2nd position of the array) once they are click. I create statements if they are selected display the card face image representing them as the correct pair.
+
+#### Rearranging the card positions
+
+As stated before I had to use an algorithm in order to reposition cards that were in the array. I did some research and discovered the Fisher-yates shuffle algorithm which is a common way of switching array elements in random positions.
+
+#### Incorrect cards flip back
+
+If the cards were not correct they would suddenly disappear so I had to think of a way of making them flip back. I made an if statement that if the array positions did not match then it would display an image of the back of the card which I applied to all the selected cards.
+
+### Successes
+
+I am most of proud of the cards being shuffled and being able to match or not match to the corresponding card. The first attempt taking a look at other examples online however their code was very completed to understand. So I broke it down into small steps gradually getting the feature to be in action.
+
+## Website Results
+
+![alt text](img/web1.png "Wireframe") ![alt text](img/web2.png "Wireframe") ![alt text](img/web3.png "Wireframe") ![alt text](img/web4.png "Wireframe")
--- js/app.js
+++ js/app.js
@@ -1,10 +1,5 @@
 $(document).ready(function() { //When the page loads
 
-
-
-  //newGame();
-
-
   var cards = document.getElementsByClassName("card");
   //The variable cards gets the class name "cards" where the list in the HTML where the class names will be stored in
 
@@ -12,126 +7,123 @@ $(document).ready(function() { //When the page loads
 
   var score = 0;
 
-  var tiles_flipped = 0;
-
   var count;
 
+  //Fisher-yates shuffle method
   function shuffle(array) {
-  var currentIndex = array.length, temporaryValue, randomIndex;
+    var currentIndex = array.length,
+      temporaryValue, randomIndex; //Gets the current array
 
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) { //While the array is not equalled to zero it goes through the array
 
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1; //Generates a random number each time the loop passes and saves in the current index
 
-    // And swap it with the current element.
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex]; //Gets the current index position stores in temporary value
+      array[currentIndex] = array[randomIndex]; //The position of the random number is grabbed by the array of the current position
+      array[randomIndex] = temporaryValue; //This switches the current index position with the array position
+    }
+
+    return array; //restores the card position back to the array
   }
 
-  return array;
-}
 
-// Used like so
-var card_array = ["card1", "card1", "card2", "card2", "card3", "card3", "card4", "card4", "card5", "card5", "card6", "card6", "card7", "card7", "card8", "card8"];
-arr = shuffle(card_array);
-var image_array = ["Arsenal-icon.png", "Arsenal-icon.png", "Burnley_icon.png", "Burnley_icon.png", "Chelsea_icon.png", "Chelsea_icon.png", "Everton_icon.png", "Everton_icon.png", "Liverpool_icon.png", "Liverpool_icon.png", "ManchesterCity_icon.png", "ManchesterCity_icon.png", "ManU_icon.png", "ManU_icon.png", "Southampton_icon.png", "Southampton_icon.png"];
-
+  var card_array = ["card1", "card1", "card2", "card2", "card3", "card3", "card4", "card4", "card5", "card5", "card6", "card6", "card7", "card7", "card8", "card8"]; //Each card element will contain one of these elements which will be used as ID's
+  arr = shuffle(card_array);
 
   document.getElementById("score").innerHTML = " Your score :" + score; //Gets a score that has been counted up
 
+  for (var i = 0; i < cards.length; i++) { //loop which goes through the card array
+    cards[i].setAttribute("id", card_array[i]); //sets each with an with an ID from the card_array
+    cards.className += (" selected"); //confirms if the card has been selected
 
 
-  // function newGame() {
-  //     tiles_flipped = 0;
-  //     var output = '';
-  //     array.shuffle();
-  //     for (var i = 0; i < array.length; i++) {
-  //       output += '<div id="card'+i+'" cards[i].addEventListener(this, \ '' +card_array[i]+'\')"><div>';
-  //     }
-  //
-  // }
 
-
-  for (var i = 0; i < cards.length; i++) {
-    cards[i].setAttribute("id", card_array[i]);
-    cards.className += (" selected");
-
-    //For loop that looks in the card list and looks for how many there are.
-
-    //Sets the cards with an id from the card_array list
-
-
-    cards[i].addEventListener('click', function(e) {
+    cards[i].addEventListener('click', function(e) { //On the event of a click on a card
       // console.log(e.target);
-      if(this.id == "card1") {
-        this.style.backgroundImage = "url('./img/Arsenal-icon.png')";
-      } else if(this.id == "card2") {
-        this.style.backgroundImage = "url('./img/Burnley_icon.png')";
-      }
-      else if(this.id == "card3") {
-        this.style.backgroundImage = "url('./img/Chelsea_icon.png')";
-      }
-      else if(this.id == "card4") {
-        this.style.backgroundImage = "url('./img/Everton_icon.png')";
-      }
-      else if(this.id == "card5") {
-        this.style.backgroundImage = "url('./img/Liverpool_icon.png')";
-      }
-      else if(this.id == "card6") {
-        this.style.backgroundImage = "url('./img/ManchesterCity_icon.png')";
-      }
-      else if(this.id == "card7") {
-        this.style.backgroundImage = "url('./img/ManU_icon.png')";
-      }
-      else if(this.id == "card8") {
-        this.style.backgroundImage = "url('./img/Southampton_icon.png')";
+      if (this.id == "card1") { //Goes to the first card element from the list
+        this.style.backgroundImage = "url('../img/Arsenal-icon.png')"; //Styles the first element with the arsenal logo
+      } else if (this.id == "card2") {
+        this.style.backgroundImage = "url('../img/Burnley_icon.png')";
+      } else if (this.id == "card3") {
+        this.style.backgroundImage = "url('../img/Chelsea_icon2.png')";
+      } else if (this.id == "card4") {
+        this.style.backgroundImage = "url('../img/Everton_icon.png')";
+      } else if (this.id == "card5") {
+        this.style.backgroundImage = "url('../img/Liverpool_icon.png')";
+      } else if (this.id == "card6") {
+        this.style.backgroundImage = "url('../img/ManchesterCity_icon.png')";
+      } else if (this.id == "card7") {
+        this.style.backgroundImage = "url('../img/ManU_icon.png')";
+      } else if (this.id == "card8") {
+        this.style.backgroundImage = "url('../img/Southampton_icon.png')";
       }
       array.push(this);
       // Every click is pushing the current card into the blank array
 
       // If cards match
       if (array.length == 2 && array[0].id == array[1].id) {
+        //if one and a second of cards are selected of the array
         // cards stay face up, increase the score and clear the array
         score++;
         document.getElementById("score").innerHTML = "Your Score" + ":" + score;
         array = [];
 
-    } else if(array.length == 2){
+      } else if (array.length == 2) {
         // turn cards face down and clear the array
         var startTime = setTimeout(function() {
-          console.log(array[0]);
-          array[0].style.backgroundImage = "url('img/card_back.jpg')";
+          array[0].style.backgroundImage = "url('../img/card_back.jpg')"; //Sets the background the the back of the card
           array[0].innerHTML = "";
-          array[1].style.backgroundImage = "url('img/card_back.jpg')";
+          array[1].style.backgroundImage = "url('../img/card_back.jpg')";
           array[1].innerHTML = "";
           array = [];
         }, 1000)
       }
 
       if (score === 8) {
-        console.log("You are the winner");
+        //Removes all the cards from the container
         $('.container').empty();
-        $('.animate-win').animate({left: '250px'});
+        $('.animate-win').animate({
+          left: '250px'
+        }); //Calls the id from the html and activates a jquery animate slide the element from the left
         var banner = $(".animate-win");
+        $('.container').empty();
 
-  for (var i=0; i<999; i++) {
+        //Creates text
+        var paragraph = document.getElementById("p");
+        var text = document.createTextNode("You Win!!");
 
-    banner.css("opacity", "1");
-    banner.animate({left: 400}, 1000);
+        // 1. Create the button
+        paragraph.appendChild(text);
+        var button3 = document.createElement("button");
+        button3.innerHTML = "Play game";
 
-    for (var k=0; k<5; k++) {
-      banner.animate({opacity: 0}, 600);
-      banner.animate({opacity: 1}, 600);
-    }
+        // 2. Append somewhere
+        var body = document.getElementsByTagName("body")[0];
+        body.appendChild(button);
 
-    banner.delay(2000).animate({left: 1200}, 1000);
-    banner.animate({left: -600}, 0);
+        // 3. Add event handler
+        button3.addEventListener("click", function() {
+          window.location.href = "game.html";
+        });
 
-  }
+        // 1. Create the button
+        var button4 = document.createElement("button");
+        button4.innerHTML = "Return home";
+
+        // 2. Append somewhere
+        var body = document.getElementsByTagName("body")[0];
+        body.appendChild(button4);
+
+        // 3. Add event handler
+        button4.addEventListener("click", function() {
+          window.location.href = "index.html";
+        });
+
+
 
 
         clearTimeout(timer);
@@ -149,85 +141,80 @@ var image_array = ["Arsenal-icon.png", "Arsenal-icon.png", "Burnley_icon.png", "
 
   }
 
-  function assignCards() {
-    for (var i = x.length - 1; i > 0; i--) {
-      cards[i].setAttribute(x[rand]);
-      //Sets the random position to the original card list
-    }
-
-  }
-
-
-
 
   //TIMER!!
-  var count = 5;
+  var count = 60; //sets the timer to 60 seconds
 
   var counter = setInterval(timer, 1000); //1000 will run it every 1 second
 
-  // if (score == 24) {
-  //   alert("You win");
-  // }
-
   function timer() {
-    count = count - 1;
+    count = count - 1; //countdown
     if (count === 0) {
       clearInterval(counter); //Clear interval stops the countdown of the set interval
       //counter ended, do something here
       console.log("Game over");
       $('.container').empty();
-      $('.lose').animate({left: '250px'});
+      $('.lose').animate({
+        left: '250px'
+      });
       var banner = $(".animate");
 
-for (var i=0; i<999; i++) {
+      for (var i = 0; i < 999; i++) {
 
-  banner.css("opacity", "1");
-  banner.animate({left: 400}, 1000);
+        banner.css("opacity", "1");
+        banner.animate({
+          left: 400
+        }, 1000);
 
-  for (var k=0; k<5; k++) {
-    banner.animate({opacity: 0}, 600);
-    banner.animate({opacity: 1}, 600);
-  }
+        for (var k = 0; k < 5; k++) {
+          banner.animate({
+            opacity: 0
+          }, 600);
+          banner.animate({
+            opacity: 1
+          }, 600); //blinks
+        }
 
-  banner.delay(2000).animate({left: 1200}, 1000);
-  banner.animate({left: -600}, 0);
+        banner.delay(2000).animate({
+          left: 1200
+        }, 1000);
+        banner.animate({
+          left: -600
+        }, 0); //animation slide off
 
-}
+      }
 
-// 1. Create the button
-var button = document.createElement("button");
-button.innerHTML = "Retry game";
+      // 1. Create the button
+      var button = document.createElement("button");
+      button.innerHTML = "Retry game";
 
-// 2. Append somewhere
-var body = document.getElementsByTagName("body")[0];
-body.appendChild(button);
+      // 2. Append somewhere
+      var body = document.getElementsByTagName("body")[0];
+      body.appendChild(button);
 
-// 3. Add event handler
-button.addEventListener ("click", function() {
-    window.location.href = "game.html";
-});
+      // 3. Add event handler
+      button.addEventListener("click", function() {
+        window.location.href = "game.html";
+      });
 
-// 1. Create the button
-var button2 = document.createElement("button");
-button2.innerHTML = "Return home";
+      // 1. Create the button
+      var button2 = document.createElement("button");
+      button2.innerHTML = "Return home";
 
-// 2. Append somewhere
-var body = document.getElementsByTagName("body")[0];
-body.appendChild(button2);
+      // 2. Append somewhere
+      var body = document.getElementsByTagName("body")[0];
+      body.appendChild(button2);
 
-// 3. Add event handler
-button2.addEventListener ("click", function() {
-    window.location.href = "index.html";
-});
+      // 3. Add event handler
+      button2.addEventListener("click", function() {
+        window.location.href = "../index.html";
+      });
 
     }
 
     //Do code for showing the number of seconds here
-    document.getElementById("timer").innerHTML = count + " secs"; // watch for spelling
+    document.getElementById("timer").innerHTML = count + " secs"; //Displays the amount of seconds in the html
   }
 
-
-
-
-  shuffle(card_array);
+  shuffle(card_array); //performs shuffle function
 });
